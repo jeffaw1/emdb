54ef7df6ae2b4b532f06fdc4fc6a17c43720fb0a
jeffaw1 <jeffwasmyname1@gmail.com>
2024-09-03 14:54:08 +0200

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -23,9 +23,11 @@ if __name__ == "__main__":
     # Search for all the test sequences on which we evaluated the baselines in the paper.
     all_emdb_pkl_files = glob.glob(os.path.join(EMDB_ROOT, "*/*/*_data.pkl"))
     emdb1_sequence_roots = []
+    print('EMDB_ROOT', EMDB_ROOT)
+    print('all_emdb_pkl_files', all_emdb_pkl_files)
     for emdb_pkl_file in all_emdb_pkl_files:
-        if is_emdb1(emdb_pkl_file):
-            emdb1_sequence_roots.append(os.path.dirname(emdb_pkl_file))
+        #if is_emdb1(emdb_pkl_file):
+        emdb1_sequence_roots.append(os.path.dirname(emdb_pkl_file))
 
     # Select the baselines we want to evaluate.
     baselines_to_evaluate = [HYBRIK, SCOREHMR]
@@ -33,4 +35,7 @@ if __name__ == "__main__":
 
     # Run the evaluation.
     evaluator_public = EvaluationEngine(compute_metrics)
+    print('emdb1_sequence_roots',emdb1_sequence_roots)
+    print('args.result_root', args.result_root)
+    print('baselines_to_evaluate', baselines_to_evaluate)
     evaluator_public.run(emdb1_sequence_roots, args.result_root, baselines_to_evaluate)
