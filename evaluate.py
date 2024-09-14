f9390119026a99f0fee3888c7874c5d3e2a85bf6
jeffaw1 <jeffwasmyname1@gmail.com>
2024-09-14 12:01:02 +0200

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -7,7 +7,7 @@ import os
 import pickle as pkl
 
 from configuration import EMDB_ROOT
-from evaluation_engine import HYBRIK, SCOREHMR, NIKI, TRAM, NLF, EvaluationEngine
+from evaluation_engine import HYBRIK, SCOREHMR, NIKI, TRAM, NLF, PLIKS, EvaluationEngine
 from evaluation_metrics import compute_metrics
 
 if __name__ == "__main__":
@@ -31,7 +31,7 @@ if __name__ == "__main__":
 
     # Select the baselines we want to evaluate.
     #baselines_to_evaluate = [HYBRIK, SCOREHMR]
-    baselines_to_evaluate = [SCOREHMR, NIKI, TRAM, NLF]
+    baselines_to_evaluate = [SCOREHMR, NIKI, TRAM, NLF, PLIKS]
 
     # Run the evaluation.
     evaluator_public = EvaluationEngine(compute_metrics)
